abd809775d26fcef633bbbcb5d2dfbfe1bc2a2ca
JiaWeiHsu <bryan35818363680919@gmail.com>
2018-12-31 19:45:06 +0800

    complete table

--- public/js/list.js
+++ public/js/list.js
@@ -40,31 +40,44 @@ $("#joinTeam").click(function () {
 });
 
 socket.on("updateTeams", allTeams => {
-    // var str = "";
-    // var str2 = "";
-    // for (var i in allTeams.allTeams) {
-    //     str += "<tr>";
-    //     str += "<td>" + allTeams.allTeams[i].teamName + "</td>";
-    //     str2 += "<option>" + allTeams.allTeams[i].teamName + "</option>";
-    //     for (var j in allTeams.allTeams[i].users) {
-    //         str += "<td>" + allTeams.allTeams[i].users[j].userName + "</td>";
-    //         str += "<td>" + allTeams.allTeams[i].users[j].position + "</td>";
-    //     }
-    //     str += "</tr>";
-    // }
-    // $("#allTeams").html(str);
-    // $("#allTeams2").html(str2);
-    // var str = "<tr><th>Teams</th><th>Factory</th><th>Distribution</th><th>Wholesaler</th><th>Retailer</th></tr>";
-
-    // allTeams 存所有的隊伍，用 for 調出所有隊伍的資料
-    for (var i in allTeams.allTeams) {
-        $("#teamList").append("<th>" + allTeams.allTeams[i].teamName + "</th>");
-        // for (let j = 0; j < 4; j++)
-        //     $("#teamList").rows[1].append(j);
-
+    var tbl = `<tr>
+                <th>Teams</th>
+                <th>Factory</th>
+                <th>Distribution</th>
+                <th>Wholesaler</th>
+                <th>Retailer</th>
+                </tr>`;
+    var str = ''
+    if (allTeams.allTeams.length > 0) {
+        for (var i in allTeams.allTeams) {
+            var Factory = "x";
+            var Distribution = "x";
+            var Wholesaler = "x";
+            var Retailer = "x";
+            for (var j in allTeams.allTeams[i].users) {
+                if (allTeams.allTeams[i].users[j].position === "Factory") {
+                    Factory = allTeams.allTeams[i].users[j].userName;
+                }
+                else if (allTeams.allTeams[i].users[j].position === "Distribution") {
+                    Distribution = allTeams.allTeams[i].users[j].userName;
+                }
+                else if (allTeams.allTeams[i].users[j].position === "Wholesaler") {
+                    Wholesaler = allTeams.allTeams[i].users[j].userName;
+                }
+                else if (allTeams.allTeams[i].users[j].position === "Retailer") {
+                    Retailer = allTeams.allTeams[i].users[j].userName;
+                }
+            }
+            str += `<tr>
+                <td>${allTeams.allTeams[i].teamName}</td>
+                <td>${Factory}</td>
+                <td>${Distribution}</td>
+                <td>${Wholesaler}</td>
+                <td>${Retailer}</td>
+                </tr>`;
+        }
     }
-    $("#teamList").rows[0].cells[0].val("123");
-
+    $("#teamList").html(tbl + str);
 });
 
 socket.on("continue", function (obj) {
