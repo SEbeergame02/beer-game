5d5ef7e3cccbc3008d7b9d889e9ca7709c6e6e07
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-01-01 19:32:21 +0800

    update playRoom

--- public/js/playRoom.js
+++ public/js/playRoom.js
@@ -1,13 +1,13 @@
 var socket = io({ transports: ["websocket"], upgrade: false });
-socket.on("putImg", allTeams => {
+socket.on("findRole", allTeams => {
     for (var i in allTeams.allTeams) {
         for (var j in allTeams.allTeams[i].users) {
             curUser = $("#userName").html().replace(/^\s+|\s+$/g, '');
             if (allTeams.allTeams[i].users[j].userName === curUser) {
                 getImg = '../img/' + allTeams.allTeams[i].users[j].position + '2.jpg';
                 $("#F").attr("src", getImg);
+                $("#role").html(allTeams.allTeams[i].users[j].position);
             }
         }
     }
-
 });
