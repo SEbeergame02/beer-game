5845c8a66fdc4ab0bf033a3f4df3e71be1c91c9e
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-01-04 17:04:42 +0800

    update register.ejs and add teamname to playRoom

--- public/js/playRoom.js
+++ public/js/playRoom.js
@@ -1,13 +1,14 @@
 var socket = io({ transports: ["websocket"], upgrade: false });
+var curUser = $("#userName").html().replace(/^\s+|\s+$/g, '');
 // 對應使用者的角色顯示不同標題和圖片
 socket.on("findRole", allTeams => {
     for (var i in allTeams.allTeams) {
         for (var j in allTeams.allTeams[i].users) {
-            curUser = $("#userName").html().replace(/^\s+|\s+$/g, '');
             if (allTeams.allTeams[i].users[j].userName === curUser) {
                 getImg = '../img/' + allTeams.allTeams[i].users[j].position + '2.jpg';
                 $("#F").attr("src", getImg);
                 $("#role").html(allTeams.allTeams[i].users[j].position);
+                $("#teamName").html(allTeams.allTeams[i].teamName);
             }
         }
     }
